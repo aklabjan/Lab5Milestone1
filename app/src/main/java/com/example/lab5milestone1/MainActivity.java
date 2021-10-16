43cbb21be5fba331045116a05d28a719be97fb3a
Ana Klabjan <39418477+aklabjan@users.noreply.github.com>
2021-10-16 18:31:59 -0500

    Milestone 1 Complete

--- app/src/main/java/com/example/lab5milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab5milestone1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lab5milestone1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -12,10 +14,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        String usernameKey = "username";
+        SharedPreferences sharedPreferences = getSharedPreferences("Lab_5_Milestone_1", Context.MODE_PRIVATE);
+       if(!sharedPreferences.getString(usernameKey,"").equals("")){
+            goToActivity2();
+       } else {
+           setContentView(R.layout.activity_main);
+       }
     }
 
     public void clickFunction(View view){
+        EditText usernameText = (EditText)findViewById(R.id.usernameText);
+        String username = usernameText.getText().toString();
+        EditText passwordText = (EditText)findViewById(R.id.passwordText);
+        String password = passwordText.getText().toString();
+        SharedPreferences sharedPreferences = getSharedPreferences("Lab_5_Milestone_1", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("username", username).apply();
+        sharedPreferences.edit().putString("password", password).apply();
         goToActivity2();
     }
 
--- app/src/main/java/com/example/lab5milestone1/MainActivity2.java
+++ app/src/main/java/com/example/lab5milestone1/MainActivity2.java
@@ -2,7 +2,14 @@ package com.example.lab5milestone1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +17,37 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("Lab_5_Milestone_1", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "");
+
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText("Welcome "+ username + "!");
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                SharedPreferences sharedPreferences = getSharedPreferences("Lab_5_Milestone_1", Context.MODE_PRIVATE);
+                sharedPreferences.edit().remove("username").apply();
+                sharedPreferences.edit().remove("password").apply();
+                goToMainActivity();
+                return true;
+            case R.id.addNotes:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void goToMainActivity(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
